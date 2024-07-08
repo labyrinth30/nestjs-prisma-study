7bc89306e584227a3d846da10904515e5f0fd0f0
younha00 <ayeon0@office.skhu.ac.kr>
2024-07-09 02:31:04 +0900

    기본적인 CRUD 구현하기

--- src/todo/todo.service.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from '@nestjs/common';
-// import { PrismaClient } from '@prisma/client'
-// const prisma = new PrismaClient()
-
-@Injectable()
-export class TodoService {}
---
+++ src/todos/todos.service.ts
@@ -0,0 +1,55 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
+import { Todo } from '@prisma/client';
+import { PrismaService } from '../prisma.client';
+
+@Injectable()
+export class TodosService {
+  constructor(private prismaService: PrismaService) {}
+
+  async create(createTodoDto: CreateTodoDto): Promise<Todo> {
+    const todo = await this.prismaService.todo.create({
+      data: {
+        title: createTodoDto.title,
+      },
+    });
+    return todo;
+  }
+
+  async findAll(): Promise<Todo[]> {
+    return this.prismaService.todo.findMany();
+  }
+
+  async findOne(id: number): Promise<Todo> {
+    const todo = await this.prismaService.todo.findUnique({
+      where: {
+        id,
+      },
+    });
+    if (!todo) {
+      throw new NotFoundException('Todo not found');
+    }
+    return todo;
+  }
+
+  async update(id: number, updateTodoDto: UpdateTodoDto): Promise<Todo> {
+    const updatedTodo = await this.prismaService.todo.update({
+      where: {
+        id,
+      },
+      data: {
+        title: updateTodoDto.title,
+      },
+    });
+    return updatedTodo;
+  }
+
+  async remove(id: number): Promise<Todo> {
+    return this.prismaService.todo.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
